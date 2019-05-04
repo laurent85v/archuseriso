7fb3177107df2d067cdbea87b5c14ae4d37b03e2
Laurent Jourden <laurent85@enarel.fr>
2019-05-04 18:37:53 +0200

    new language it_IT

--- configs/kde/build.sh
+++ configs/kde/build.sh
@@ -40,7 +40,7 @@ _usage ()
     echo "    -o <out_dir>       Set the output directory"
     echo "                        Default: ${out_dir}"
     echo "    -l <language>      Change the default language, select one from:"
-    echo "                        de,es,fr"
+    echo "                        de,es,fr,it"
     echo "    -v                 Enable verbose output"
     echo "    -h                 This help message"
     exit ${1}
@@ -321,6 +321,7 @@ while getopts 'N:V:L:P:A:D:w:o:g:vhl:' arg; do
                 'de'|'de_DE') lang="de_DE";;
                 'es'|'es_ES') lang="es_ES";;
                 'fr'|'fr_FR') lang="fr_FR";;
+                'it'|'it_IT') lang="it_IT";;
                 *) _usage 1;;
             esac;;
         *)
