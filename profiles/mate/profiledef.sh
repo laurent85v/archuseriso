2278503e462b89192f9de93915e7eb7ea2e5b63e
Laurent Jourden <laurent85@enarel.fr>
2020-12-12 17:47:04 +0100

    allow specifying ownership and mode of custom airootfs
    Refers to archiso commit 42d9e4f9:
    profiledef.sh can now contain an associative array called file_permissions
    which can be used to set custom ownership and mode of custom airootfs files.
    The array's keys contain the path and the value is a colon separated list of
    owner UID, owner GID and access mode.
    For example:
    
        file_permissions=(
          ["/etc/shadow"]="0:0:400"
        )

--- profiles/mate/profiledef.sh
+++ profiles/mate/profiledef.sh
@@ -13,3 +13,11 @@ arch="x86_64"
 pacman_conf="pacman.conf"
 pacman_testing_conf="pacman-testing.conf"
 airootfs_image_tool_options=('-comp' 'zstd')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:750"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+)
