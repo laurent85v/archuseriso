10835bef18a0ed2910ae993d354570ab94c67573
Laurent Jourden <laurent85@enarel.fr>
2022-09-09 09:41:45 +0200

    Add grub x64 boot modes in profiledef.sh

--- profiles/gnome/profiledef.sh
+++ profiles/gnome/profiledef.sh
@@ -10,10 +10,10 @@ iso_version=""
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-ia32.grub.esp' 'uefi-x64.systemd-boot.esp'
-           'uefi-ia32.grub.eltorito' 'uefi-x64.systemd-boot.eltorito'
-           'uefi-x64.refind.esp' 'uefi-x64.refind.eltorito')
-# default x64 EFI boot loader: "uefi-x64.refind.esp" or "uefi-x64.systemd-boot.esp"
+           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
+           'uefi-x64.refind.esp' 'uefi-x64.systemd-boot.esp'
+           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito'
+           'uefi-x64.refind.eltorito' 'uefi-x64.systemd-boot.eltorito')
 x64_uefi_default_bootloader="uefi-x64.refind.esp"
 arch="x86_64"
 pacman_conf="pacman.conf"
