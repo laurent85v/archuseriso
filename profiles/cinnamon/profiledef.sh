37985f3d8ff9558dcc2e7a9777b7de96f02d918c
Laurent Jourden <laurent85@enarel.fr>
2022-10-21 10:48:22 +0200

    change default uefi bootloader to systemd-boot
    rEFInd boot manager fails to display main menu screen on some firmware

--- profiles/cinnamon/profiledef.sh
+++ profiles/cinnamon/profiledef.sh
@@ -14,7 +14,7 @@ bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
 	   'uefi-x64.refind.esp' 'uefi-x64.systemd-boot.esp'
            'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito'
 	   'uefi-x64.refind.eltorito' 'uefi-x64.systemd-boot.eltorito')
-x64_uefi_default_bootloader="uefi-x64.refind.esp"
+x64_uefi_default_bootloader="uefi-x64.systemd-boot.esp"
 arch="x86_64"
 pacman_conf="pacman.conf"
 pacman_testing_conf="pacman-testing.conf"
