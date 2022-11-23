9650cc6354cb8f9b00f06a45f64fbf765a57d18a
Laurent Jourden <laurent85@enarel.fr>
2022-11-23 07:38:50 +0100

    mediumdef.sh: partition suffix to lowercase, new medium version

--- profiles/xfce/drive/mediumdef.sh
+++ profiles/xfce/drive/mediumdef.sh
@@ -2,20 +2,21 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+arch="x86_64"
 aui_suffix="$(mktemp -u _XXX)"
-cow_label="COW${aui_suffix:-}"
-crypt_label="LUKS${aui_suffix:-}"
-crypt_mapper="LUKS${aui_suffix:-}"
-esp_label="ESP${aui_suffix:-}"
+cow_label="COW${aui_suffix,,}"
+crypt_label="LUKS${aui_suffix,,}"
+crypt_mapper="LUKS${aui_suffix,,}"
+esp_label="ESP${aui_suffix,,}"
 desktop="%DESKTOP%"
-img_label="%ARCHISO_LABEL%${aui_suffix:-}"
+img_label="%ARCHISO_LABEL%${aui_suffix,,}"
 install_dir="%INSTALL_DIR%"
 iso_label="%ARCHISO_LABEL%"
 iso_name="%ISO_NAME%"
 iso_version="%ISO_VERSION%"
 lang="%LANG%"
-medium_version='v6'
-root_label=ROOT${aui_suffix:-}
+medium_version='v7'
+root_label=ROOT${aui_suffix,,}
 cow_files_settings=(
         "persistent_${img_label}/x86_64/upperdir/etc/fstab")
 esp_files_settings=(
