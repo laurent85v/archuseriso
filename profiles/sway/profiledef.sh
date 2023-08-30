b9625c81d20309409d4956af8ccdc82f1868ce3b
Laurent Jourden <laurent85@enarel.fr>
2023-08-30 22:07:29 +0200

    use pcsclite for communicating with OpenPGP smartcards
    archiso commit 6c5ab3d3:
    As opgpcard uses pcsclite and gnupg is able to use it as well, switch
    away from using gnupg's internal ccid driver.

--- profiles/sway/profiledef.sh
+++ profiles/sway/profiledef.sh
@@ -23,6 +23,7 @@ file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:750"
+  ["/root/.gnupg"]="0:0:700"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
 )
