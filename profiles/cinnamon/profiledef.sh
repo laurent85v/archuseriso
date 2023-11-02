91c605daf4b8c3d671f611ba38477e73bf0ccd7e
Laurent Jourden <laurent85@enarel.fr>
2023-11-02 21:23:29 +0100

    add systemd-boot ia32 boot loader

--- profiles/cinnamon/profiledef.sh
+++ profiles/cinnamon/profiledef.sh
@@ -9,11 +9,20 @@ iso_application="Archuseriso ${desktop} Live/Rescue medium"
 iso_version=""
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
-	   'uefi-x64.refind.esp' 'uefi-x64.systemd-boot.esp'
-           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito'
-	   'uefi-x64.refind.eltorito' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux.mbr'
+           'bios.syslinux.eltorito'
+           'uefi-ia32.grub.eltorito'
+           'uefi-ia32.grub.esp'
+           'uefi-x64.grub.eltorito'
+           'uefi-x64.grub.esp'
+           'uefi-x64.refind.eltorito'
+           'uefi-x64.refind.esp'
+           'uefi-ia32.systemd-boot.eltorito'
+           'uefi-ia32.systemd-boot.esp'
+           'uefi-x64.systemd-boot.esp'
+           'uefi-x64.systemd-boot.eltorito'
+           )
+ia32_uefi_default_bootloader="uefi-ia32.systemd-boot.esp"
 x64_uefi_default_bootloader="uefi-x64.systemd-boot.esp"
 arch="x86_64"
 pacman_conf="pacman.conf"
